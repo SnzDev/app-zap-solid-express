712913475c1ddec2afbca669d0b61a2f2d34fe5e
SnzDev <tms.working@gmail.com>
2022-10-12 15:08:44 -0300

    Adcionado rota e controller da função destroy

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { initializeAllInstancesController } from "./use-cases/initialize-all-instances";
 import { getStatusInterfaceController } from "./use-cases/get-status-interface";
 import { initializeInstanceController } from "./use-cases/initialize-instance";
+import { destroyInstanceController } from "./use-cases/destroy-instance";
 
 const companyRoutes = Router();
 
@@ -17,6 +18,10 @@ companyRoutes.get("/status/:access_key", async (request, response) => {
 
 companyRoutes.get("/init/:access_key", async (request, response) => {
   await initializeInstanceController.handle(request, response);
+});
+
+companyRoutes.get("/destroy/:access_key", async (request, response) => {
+  await destroyInstanceController.handle(request, response);
 });
 
 export { companyRoutes };
