901c1700ba70128fb8635a6ae9bafafce9dad862
SnzDev <tms.working@gmail.com>
2022-10-21 18:12:06 -0300

    refatorado destroy instance

--- src/use-cases/destroy-instance/destroy-instance-controller.ts
+++ src/use-cases/destroy-instance/destroy-instance-controller.ts
@@ -2,17 +2,11 @@ import { Request, Response } from "express";
 import { DestroyInstanceUseCase } from "./destroy-instance-use-case";
 
 export class DestroyInstanceController {
-  constructor(private destroyInstanceUseCase: DestroyInstanceUseCase) {}
-
   async handle(request: Request, response: Response) {
     const { access_key } = request.params;
-    try {
-      await this.destroyInstanceUseCase.execute(access_key);
-      return response.status(200).send();
-    } catch (error: any) {
-      return response
-        .status(error.getStatusCode())
-        .json({ msg: error.message });
-    }
+
+    const destroyInstanceUseCase = new DestroyInstanceUseCase();
+    await destroyInstanceUseCase.execute(access_key);
+    return response.status(200).send();
   }
 }
